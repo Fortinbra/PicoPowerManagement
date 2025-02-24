34328e184e4e0c48ffc6d10763e04fa3ae76db35
Fortinbra <fortinbra@becauseimclever.com>
2025-02-23 19:58:11 -0600

    Add initial implementation of PicoPowerManagement project with API and client setup

---
+++ app/src/PicoPowerManagement.Api/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+
+namespace PicoPowerManagement.Api
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration); // Corrected parameters
+
+
+			builder.Services.AddProblemDetails();
+
+			var app = builder.Build();
+
+			app.UseWebAssemblyDebugging(); // Corrected method name
+			app.UseDeveloperExceptionPage();
+			app.UseStatusCodePages();
+			app.UseBlazorFrameworkFiles();
+			app.UseStaticFiles();
+			app.MapFallbackToFile("index.html");
+			app.Run();
+		}
+	}
+}
